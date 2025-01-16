e3a5446c824a01a33ad53dc0ebbbe5b5443d08ac
HOLDEN WEBER <holdenweber38@gmail.com>
2025-01-16 13:11:39 -0500

    Selected Date Completed

---
+++ Lab1/SelectedDate.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SelectedDate.aspx.cs" Inherits="Lab1.SelectedDate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Date</title>
+    
+</head>
+<body>
+    <h1>My First ASP Web Page</h1>
+    <asp:Label ID="lblDisplay" runat="server" Text="Label"></asp:Label>
+    <asp:Label ID="lblTest" runat="server" Text="Label"></asp:Label>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="First Name: "></asp:Label>
+            <asp:TextBox ID="txtfname" runat="server"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Last Name: "></asp:Label>
+            <asp:TextBox ID="txtlname" runat="server"></asp:TextBox>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+            <asp:Button ID="Button1" runat="server" Text="Submit" />
+            <asp:Button ID="Button2" runat="server" Text="Clear" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab1/SelectedDate.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab1
+{
+    public partial class SelectedDate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Calendar1.DayRender += new DayRenderEventHandler(Calendar1_DayRender);
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            string strLastName, strFirstName, strFullName, myDate;
+            strFirstName = txtfname.Text.Trim();
+            strLastName = txtlname.Text.Trim();
+            strFullName = strFirstName + " " + strLastName;
+
+            if (this.Calendar1.SelectedDates.Count == 1)
+            {
+                myDate = "Selected date: " + Calendar1.SelectedDate;
+            }
+            else
+            {
+                myDate = "Selected dates: " + Calendar1.SelectedDates[0] + " to " + Calendar1.SelectedDates[Calendar1.SelectedDates.Count - 1];
+            }
+
+            lblDisplay.Text = strFullName + "<br/>" + myDate;
+
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            e.Cell.VerticalAlign = VerticalAlign.Top;
+
+            if (e.Day.DayNumberText == "7")
+            {
+                e.Cell.BorderColor = System.Drawing.Color.SpringGreen;
+                e.Cell.BorderStyle = BorderStyle.Groove;
+                e.Cell.Controls.Add(new LiteralControl("<p><i>Second day of class</i></p>"));
+            }
+        }
+
+    }
+}
